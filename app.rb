7feeccda1bcdf2a1a2d4dd21005e185443a6a1f7
Veronica Lopez <nicaa0695@gmail.com>
2019-08-23 19:41:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Nica!"
+  end 
+  get '/hometown' do 
+    "My hometown is Albuquerque!"
+  end 
+  get '/favorite-song' do
+  "My favorite song is Claim by DVSN!"
+  end
 end
